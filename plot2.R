1b5e789fc16c784657c68088e805ae77e4554cc0
Big-Nurpin <sdean.jeremy@gmail.com>
2020-09-14 19:49:30 -0700

    Initial commit

---
+++ plot2.R
@@ -0,0 +1,6 @@
+##Line graph of Global Active Power of the two days
+
+##Download and unzip the data file
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold
+              _power_consumption.zip", destfile = "data.zip")
+unzip("data.zip")
