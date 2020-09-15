2a91278cf023c29249b42e7eefbc54b3458b912e
Big-Nurpin <sdean.jeremy@gmail.com>
2020-09-14 19:51:54 -0700

    Initial Commit

---
+++ plot3.R
@@ -0,0 +1,6 @@
+## Line graph showing the sub-metering in different colors
+
+##Download and unzip the data file
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold
+              _power_consumption.zip", destfile = "data.zip")
+unzip("data.zip")
